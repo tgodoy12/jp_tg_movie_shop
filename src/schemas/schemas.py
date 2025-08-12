dd59298fce92d9a171e0d5a7104d4b491b62280d
Trilce <trilcegodoy11@gmail.com>
2025-08-12 12:10:51 -0300

    schemas actualizados

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -2,13 +2,38 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 class Movie(BaseModel):
+    id: int
+    name: str 
+    director: str 
+    gender: List[str]
+    shop: int
+    rent: bool
 
 class MovieRequestCreate(BaseModel):
+    name: str
+    director: str
+    gender: List[str]
+    shop: int
 
 class MovieRequestUpdate(BaseModel):
+    name: str
+    director: str
+    gender: List[str]
+
+class MovieRequestUpdateShop(BaseModel):  
+    shop: int
 
 class Shop(BaseModel):
+    id: int
+    address: str
+    manager: str 
+    movies: List[Movie]
 
 class ShopRequestCreate(BaseModel):
+    address: str 
+    manager: str 
+
 
 class ShopUpdate(BaseModel):
+    address: str 
+    manager: str
